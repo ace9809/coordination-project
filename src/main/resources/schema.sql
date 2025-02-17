b59bb6f0a4fefcf969bfab79dc1e6d5eaf7408f2
Ace <ace@ace-E01.local>
2025-02-17 23:51:30 +0900

    chore: application yml 수정

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,45 @@
+DROP TABLE IF EXISTS product_category_statistics;
+DROP TABLE IF EXISTS product_brand_statistics;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS brands;
+
+CREATE TABLE brands
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(25) NOT NULL,
+    created_at DATETIME(6) NULL COMMENT '생성일자',
+    updated_at DATETIME(6) NULL COMMENT '변경일자'
+);
+
+CREATE TABLE products
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    category   VARCHAR(255) NOT NULL,
+    brand_id   BIGINT       NOT NULL,
+    price      BIGINT       NOT NULL,
+    created_at DATETIME(6) NULL COMMENT '생성일자',
+    updated_at DATETIME(6) NULL COMMENT '변경일자'
+);
+
+CREATE TABLE product_brand_statistics
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    brand_id    BIGINT NOT NULL,
+    total_price BIGINT NOT NULL,
+    created_at  DATETIME(6) NULL COMMENT '생성일자',
+    updated_at  DATETIME(6) NULL COMMENT '변경일자'
+);
+
+CREATE TABLE product_category_statistics
+(
+    id             BIGINT AUTO_INCREMENT PRIMARY KEY,
+    category       VARCHAR(255) NOT NULL,
+    min_brand_id   BIGINT       NOT NULL,
+    min_product_id BIGINT       NOT NULL,
+    min_price      BIGINT       NOT NULL,
+    max_brand_id   BIGINT       NOT NULL,
+    max_product_id BIGINT       NOT NULL,
+    max_price      BIGINT       NOT NULL,
+    created_at     DATETIME(6) NULL COMMENT '생성일자',
+    updated_at     DATETIME(6) NULL COMMENT '변경일자'
+);
